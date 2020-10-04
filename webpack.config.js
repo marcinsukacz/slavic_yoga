ad93781db0f39dc25954441bf27f38e1283fa9ce
marcinsukacz <marcin.sukacz@gmail.com>
2020-10-04 12:13:21 +0100

    going back to last webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,28 @@
-var path = require('path')
-var webpack = require('webpack')
-
-module.exports = {
-  entry: './src/main.js',
+const config = {
+  entry: `${__dirname}/src/app.js`,
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
+    path: `${__dirname}/public/js/`,
+<<<<<<< HEAD
+=======
+    publicPath: '/slavic_yoga/',
+>>>>>>> gh-pages
+    filename: 'bundle.js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-          }
-          // other vue-loader options go here
-        }
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
-  },
-  resolve: {
+  resolve: {   // NEW
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
-    }
+    },
+    extensions: ['*', '.js', '.vue', '.json']
   },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true
-  },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map'
-}
+  mode: 'development'
+};
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
+<<<<<<< HEAD
+// module.exports = config;
+
+module.exports = {
+  publicPath: '/name-of-your-project/'
 }
+=======
+module.exports = config;
+>>>>>>> gh-pages
